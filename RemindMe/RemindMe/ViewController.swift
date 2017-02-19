207ed8e836a3aa17102d519e2d50e9a3458a16b1
Neeraj Khede <neeraj.khede@globant.com>
2017-02-19 17:17:26 +0530

    Initial commit

---
+++ RemindMe/RemindMe/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Neeraj Khede on 27/01/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculate = Calculate();
+        print(calculate.add(x: 10, y: 5));
+        print(calculate.sub(x: 10, y: 5));
+        
+    }
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+class Calculate
+{
+    func add(x:Int,y:Int) -> Int {
+        return x+y;
+    }
+    
+    func sub(x:Int,y:Int) -> Int {
+        return x-y;
+    }
+}
+
