9483aa1cc0c9b0f30af9610eb532ebe5800f0f36
Neeraj Khede <neeraj.khede@globant.com>
2017-03-03 15:56:06 +0530

    final change

--- RemindMe/RemindMe/ShowListTableViewController.swift
+++ RemindMe/RemindMe/ShowListTableViewController.swift
@@ -20,6 +20,8 @@ class ShowListTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    //final change
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
