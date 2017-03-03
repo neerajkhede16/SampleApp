185036814beba3978a3507b989fe53e32139fa13
Neeraj Khede <neeraj.khede@globant.com>
2017-03-03 15:47:01 +0530

    cherry pick test.

--- RemindMe/RemindMe/AppDelegate.swift
+++ RemindMe/RemindMe/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //Cherry pick test
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
